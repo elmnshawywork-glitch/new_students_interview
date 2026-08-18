6268a580dc16081a1e9a08998ee0454d8cacdf35
elmnshawy.work <elmnshawy.work@gmail.com>
2026-08-18 14:35:26 +0300

    config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 // ضع رابط الـ Web App (المنتهي بـ /exec) بين علامتي التنصيص بالأسفل
 // للتشغيل الحقيقي والحفظ على Google Sheet. اتركه "" للوضع التجريبي.
 window.APP_CONFIG = {
-  BACKEND_URL: "https://script.google.com/macros/s/AKfycbyff9r94ik5_a38IyX4SmsEgYx8WTNskuekGzN4bGst8j7q9Z2cq2xfvfKcxLZwkMV2Uw/exec",   // ← الصق هنا رابط /exec
+  BACKEND_URL: "https://script.google.com/macros/s/AKfycbwXMYC_-pRUhnHw9OBY_NDV4Xm_I8b0IFT3VDyCb32mDhxQ6ymxx4dB9kCCDJ8peq_BEg/exec",   // ← الصق هنا رابط /exec
   APP_TITLE: "منظومة قبول المدارس التكنولوجية التطبيقية",
   EXAM_MINUTES: 15
 };
